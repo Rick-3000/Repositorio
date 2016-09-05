ff86716844332fa521675fc226b9678b1d7b1c71
USUARIO-PC\USUARIO <rickypilay_96@hotmail.es>
2016-09-05 18:11:54 -0500

    comit 3

---
+++ TareaINGSoFt2RickPilay/src/tareaingsoft2rickpilay/Clase2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaingsoft2rickpilay;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Clase2 {
+    
+    public void Metodo1(){
+        System.out.println("1234");
+    }
+    
+}
